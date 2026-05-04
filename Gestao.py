82d89cf7b06bcefee0af3c1012280b6686a97ee3
carlosalberto0843 <cneto0843@gmail.com>
2026-05-04 09:07:13 -0300

    prototipo_7

--- Gestao.py
+++ Gestao.py
@@ -1,7 +1,10 @@
 adm = []
 clientes = []
 animal = []
-estoque = []
+estoque_animal = []
+estoque_produtos = []
+venda_animal = []
+venda_produtos = []
 a1 = ''
 a2 = ''
 a3 = ''
@@ -58,63 +61,68 @@ while op != 0:
                             if gr == '1':
                                 l1 = input('qual o tipo de animal - [Bovino de Leite, Caprino, Ovino, Suíno/Leitão]: ')
                                 l2 = int(input('qual a identificaçao do animal? - [brinco/número]: '))
-                                l3 = input('qual o status do animal? - [em lactação, para engorda, disponível para venda]: ')
-                                animal.append([l1, l2, l3])
-                                print('animal adcionado')
-                                continue
+                                l3 = input('qual o status do animal? - [lactação, engorda, venda]: ')
+                                if l3 == 'lactação' or l3 == 'engorda':
+                                    animal.append([l1, l2, l3])
+                                    print('animal adcionado')
+                                    continue
+                                elif l3 == 'venda':
+                                    estoque_animal.append([l1, l2, l3])
+                                    print('animal adcionado')
+                                    continue
+
                             elif gr == '2':
                                 buscar = int(input('digite o numero do animal: '))
                                 for item in animal:
                                     if item[1] == buscar:  # só o ID
                                         print(f'o animal é: {item}')                     
+                        
                         elif geral == '2':
-                            
                             print('----gerenciar producao e derivados----')
-                            print('cadastre de produçao diaria - [1]')
-                            print('gerenciar estoque - [2]')
-                            print('voltar - [3]')
+                            print('cadastar produçao diaria - [1]')
+                            print('gerenciar estoque de animal  - [2]')
+                            print('gerenciar estoque de produtos - [3]')
+                            print('gerenciar itens para a venda - [4]')
+                            print('voltar - [0]')
                             escolha = int(input('selecione uma opcao: '))
                             if escolha == 1:
-                                nu = int(input('digite o numero do animal: '))
-                                for loop in animal:
-                                    if loop[1] == nu:
-                                        print(loop)
-                                        diaria = input('qual produto foi produzido: ')
-                                        diaria2 = float(input('quanto produzido: '))
-                                        diaria3 = float(input('qual o valor para venda: '))                                 
-                                        estoque.append([ nu, diaria, diaria2, diaria3])
-                                        print(estoque)
-                                
-                            elif escolha == 2:
-                                print(f'----bem vindo {login} oque deseja fazer---- ')
-                                print('ver protutos no estoque atual - [1]')
-                                print('colocar um animal para a venda - [2]')
-                                print('remover produtos do estoque - [3]')
-                                print('voltar - [5]')
-                                escolha_estoque = int(input('digite a opcao desejada: '))
-                                if escolha_estoque == 1:
-                                        for itens in estoque:
-                                            print(itens)
-                                        print('')
+                                print('----gerenciar estoque de animal----') #gerenciar estoque de animal
+                                print('ver animais em estoque - [1]')
+                                print('remover animais do estoque - [3]')
+                                print('inserir animal a venda -[4]')
+                                print('voltar - [0]')
+                                escolha_animal = int(input('digite a opcao desejada: '))
+                                if escolha_animal == 2:
+                                    for itens in estoque_animal:
+                                        print(itens)
+                                elif escolha_animal == 4:
+                                    nu = int(input('digite numero do animal: '))
+                                    achei = False
+                                    for itens in estoque_animal:
+                                        if item[1] == nu:
+                                            venda_animal.append(itens)
+                                            estoque_animal.remove(itens)
+                                            print('animal adicionado com sucesso')
+                                            achei = True
+                                            break
+                                    if not achei:
+                                        print('animal nao adicionado, digite o numero novamente')
 
-                                elif escolha_estoque == 2:
-                                    nu = int(input('digite o numero do animal: '))
-                                    for loop in animal:
-                                        if loop[1] == nu:
-                                            v1 = input('tipo de animal: ')
-                                            v2 = float(input('peso do animal:'))
-                                            v3 = float(input('valor do animal: '))
-                                            estoque.append([nu, v1 ,v2 ,v3])
-                                
-                                elif escolha_estoque == 3:
-                                        nu2 = int(input('digite o numero de animal: '))
-                                        achei = False
-                                        for ani in estoque:
-                                            if ani[0] == nu2:
-                                                estoque.remove(ani)
-                                                print('removido com sucesso')
-                                                achei = True
-                                                break
-                                                
-                                        if not achei:
-                                            print('produto nao encontrado')
+                            elif escolha == 3:
+                                print('----gerenciar estoque de produtos----') #gerenciar estoque de produtos
+                                print('ver protutos no estoque atual - [1]')
+                                print('adicionar protudos ao estoque - [2]')
+                                print('remover produtos do estoque - [3]')
+                                print('colocar protutos a venda - [4]')
+                                print('voltar - [5]')
+                                escolha_protudos = int(input('digite a opcao desejada: '))
+                                if escolha_protudos == 1:
+                                    for itens in escolha_protudos:
+                                        print(itens)
+                                    print('')
+                                elif escolha_protudos == 2:
+                                    pro1 = input('digite o nome do produto: ')
+                                    pro2 = float(input('digite a quantidade desse produto em kg ou litros: '))
+                                    pro3 = input('digite a data de hoje: ')
+                                    pro4 = int(input('digite numero para a indentificaçao: '))
+                                    estoque_produtos.append([pro1, pro2 ,pro3])
